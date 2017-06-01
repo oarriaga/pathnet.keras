7ba775c200768b4e30abb8ae9a67a402d3bff6b1
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-01 19:53:58 +0200

    Added untested mutate method

--- src/genetic_path.py
+++ src/genetic_path.py
@@ -2,19 +2,20 @@ import random
 import numpy as np
 
 class GeneticPath(object):
-    def __init__(self, shape=(3,5), population_size=64,
+    def __init__(self, shape=(3, 5), population_size=64,
                                 max_num_active_paths=3):
         self.num_modules_per_layer, self.num_layers = shape
         self.max_num_active_paths = max_num_active_paths
         self.population_size = population_size
         self.population = self.generate_population()
+        self.mutation_rate = 1.0 / (self.num_layers * self.num_active_paths)
 
     def generate_population(self):
-        population = [self._generate_individual()
+        population = [self._generate_genotype_path()
                 for _ in range(self.population_size)]
         return population
 
-    def _generate_individual(self):
+    def _generate_genotype_path(self):
         path = np.zeros(shape=(self.num_modules_per_layer, self.num_layers))
         return np.apply_along_axis(self._generate_valid_module,
                                             axis=0, arr=path)
@@ -30,11 +31,31 @@ class GeneticPath(object):
     def sample_genotype_paths(self, num_genotypes=2):
         return random.sample(self.population, num_genotypes)
 
+    def mutate(self, genotype_path):
+        mutation_probabilities = np.random.random(size=(self.num_layers,
+                                                self.num_active_paths))
+        inside_path_mask = genotype_path == 1
+        mutation_mask = self.mutation_rate > mutation_probabilities
+        # mutation is only applied to the preexisting paths.
+        mutation_mask = np.logical_and(inside_path_mask, mutation_mask)
+        # restart the to-be mutated genes.
+        genotype_path[mutation_mask] = 0
+        x_mutated_args, y_mutated_args = np.where(mutation_mask)
+        x_mutation = np.random.randint(-2 , 2, size=x_mutated_args.shape)
+        y_mutation = np.random.randint(-2 , 2, size=y_mutated_args.shape)
+        x_mutated_args = x_mutated_args + x_mutation
+        y_mutated_args = y_mutated_args + y_mutation
+        max_module_arg = self.num_modules_per_layer - 1
+        x_mutated_args = np.clip(x_mutated_args, 0, max_module_arg)
+        y_mutated_args = np.clip(y_mutated_args, 0, max_module_arg)
+        mutated_args = (x_mutated_args, y_mutated_args)
+        genotype_path[mutated_args] = 1
+        return genetic_paths
+
     def mutate_genotype_path(self, genotype_path):
         mutation_probabilities = np.random.random(size=(self.num_layers,
                                                 self.num_active_paths))
-        mutation_rate = 1.0 / (self.num_layers * self.num_active_paths)
-        mutation_mask = mutation_probabilities < mutation_rate
+        mutation_mask = self.mutation_rate > mutation_probabilities
         selected_paths = genotype_path[mutation_mask]
         selected_paths = selected_paths + np.random.randint(low=-2, high=2,
                                                 size=selected_paths.shape)
