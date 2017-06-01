b234dea16a269d05759b2289feb01661c38b12cf
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-01 13:09:59 +0200

    Changed pathnet shape

--- src/pathnet.py
+++ src/pathnet.py
@@ -5,27 +5,25 @@ from keras.models import Model
 import numpy as np
 
 class PathNet(object):
-    def __init__(self, num_layers=3, num_modules_per_layer=10,
-                    population_size=64, num_active_paths=3):
-        self.num_layers = num_layers
-        self.num_modules_per_layer = num_modules_per_layer
+    def __init__(self, shape=(3,5), population_size=64, num_active_paths=3,
+                input_size=10, output_size=20, num_neurons_per_module=20):
+        self.num_modules_per_layer, self.num_layers = shape
         self.population_size = population_size
         self.num_active_paths = num_active_paths
-        self.output_size = (10)
-        self.input_layer = Input(shape=(20,))
+        self.input_layer = Input(shape=(input_size,))
+        self.output_size = output_size
         self.output_layer = Dense(self.output_size)
-        self.num_neurons_per_module = 20
-        #self.module_paths = self._calculate_all_paths()
+        self.num_neurons_per_module = num_neurons_per_module
         self.pathnet = self._instantiate_pathnet()
 
     def _instantiate_pathnet(self):
-        layers = []
-        for layer_arg in range(self.num_layers):
-            modules = []
-            for module_arg in range(self.num_modules_per_layer):
-                modules.append(Dense(self.num_neurons_per_module))
-            layers.append(modules)
-        return np.asarray(layers)
+        modules = []
+        for module_arg in range(self.num_modules_per_layer):
+            layers = []
+            for layer_arg in range(self.num_layers):
+                layers.append(Dense(self.num_neurons_per_module))
+            modules.append(layers)
+        return np.asarray(modules)
 
     def compile_path(self, individual):
         """ pathnet should be a numpy array of size
@@ -33,17 +31,16 @@ class PathNet(object):
         """
         #path_mask = individual == True
         #selected_path = pathnet[path_mask]
-        num_layers, num_modules_per_layer = individual.shape
         reduced_sum_modules = []
-        for layer_arg in range(num_layers):
+        for layer_arg in range(self.num_layers):
             layer_paths = []
-            for module_arg in range(num_modules_per_layer):
-                if individual[layer_arg, module_arg]:
+            for module_arg in range(self.num_modules_per_layer):
+                if individual[module_arg, layer_arg]:
                     if layer_arg == 0:
-                        layer_paths.append(self.pathnet[layer_arg, module_arg](
+                        layer_paths.append(self.pathnet[module_arg, layer_arg](
                                                             self.input_layer))
                     else:
-                        layer_paths.append(self.pathnet[layer_arg, module_arg](
+                        layer_paths.append(self.pathnet[module_arg, layer_arg](
                                         reduced_sum_modules[layer_arg - 1]))
             reduced_sum_modules.append(ReduceSum(axis=-1)(layer_paths))
 
@@ -56,8 +53,8 @@ if __name__ == '__main__':
 
     num_layers = 5
     num_modules_per_layer = 10
-    pathnet = PathNet(num_layers, num_modules_per_layer)
-    individual = np.ones(shape=(num_layers, num_modules_per_layer))
+    pathnet = PathNet(shape=(num_modules_per_layer, num_layers))
+    individual = np.ones(shape=(num_modules_per_layer, num_layers))
     model = pathnet.compile_path(individual)
     plot_model(model, to_file='../images/random_pathnet.png')
 
