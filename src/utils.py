2429a5373b51c8c3e2aa74886953aa49f4a2baaa
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-01 23:35:41 +0200

    Added split data function for test/val data

--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,17 @@ def display_image(image_array, image_class, cmap=None):
     plt.title('ground truth class: ' + image_class)
     plt.show()
 
+def split_data(train_data, validation_split=.2):
+    num_train = int(round((1 - validation_split) * len(train_data[0])))
+    train_images, train_classes = train_data
+    train_images = train_images[:num_train]
+    train_classes = train_classes[:num_train]
+    validation_images = train_data[num_train:]
+    validation_classes = train_data[num_train:]
+    train_data = (train_images, train_classes)
+    validation_data = (validation_images, validation_classes)
+    return train_data, validation_data
+
 
 
 
