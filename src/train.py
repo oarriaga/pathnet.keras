4606b8ceca2ed9d83de838b779fb93a9afaaa954
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-04 00:39:40 +0200

    Found bug related to not categorical output classes

--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
+from keras.utils import to_categorical
 import numpy as np
+
 from data_manager import DataManager
 from genetic_path import GeneticPath
 from pathnet import PathNet
@@ -18,13 +20,22 @@ verbosity=1
 # instantiating classes and process data
 genetic_paths = GeneticPath(shape=(num_modules_per_layer, num_layers))
 pathnet = PathNet(shape=(num_modules_per_layer, num_layers))
+
+# loading and pre-processing dataset
 data_manager = DataManager()
-train_data, test_data = data_manager.load('svhn')
+train_data, test_data = data_manager.load('mnist', class_args=[6, 7])
 train_data, validation_data = split_data(train_data)
 train_images, train_classes = train_data
-train_images = normalize_images(train_images)
 validation_images, validation_classes = validation_data
-validation_images = normalize_images(validation_images)
+test_images, test_classes = test_data
+
+arg_classes = (train_classes, validation_classes, test_classes)
+one_hot_classes = [to_categorical(class_data) for class_data in arg_classes]
+train_classes, validation_classes, test_classes = one_hot_classes
+
+image_data = (train_images, validation_images, test_images)
+normalized_images = [normalize_images(image) for image in image_data]
+train_images, validation_images, test_images = normalize_images
 
 # train path with evolution strategies
 for genetic_epoch_arg in range(num_genetic_epochs):
@@ -36,8 +47,9 @@ for genetic_epoch_arg in range(num_genetic_epochs):
         path_model.compile(optimizer='adam', loss='categorical_crossentropy',
                                                         metrics=['accuracy'])
         path_model.fit(train_images, train_classes, batch_size, num_cnn_epochs,
-                                    verbosity, validation_split, shuffle=True)
-        score = path_model.evaluate(validation_images, validation_classes)
+                                    verbosity, validation_data=validation_data,
+                                                                  shuffle=True)
+        score = path_model.evaluate(test_images, test_classes)
         losses.append(score[0])
     losses = np.asarray(losses)
     winner_arg = np.argmax(losses)
