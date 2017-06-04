d44a672ab6e2519fd7d20029fead21d7931422e6
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-04 10:01:24 +0200

    Found bug in mutation method not performing copy and passing by reference. Added new main test

--- src/genetic_path.py
+++ src/genetic_path.py
@@ -30,8 +30,8 @@ class GeneticPath(object):
         return module
 
     def sample_genotype_paths(self, num_genotypes=2):
-        sampled_args = random.sample(np.arange(self.population_size,
-                                                    num_genotypes))
+        sampled_args = random.sample(list(range(self.population_size)),
+                                                        num_genotypes)
         sampled_individuals = []
         for sampled_arg in sampled_args:
             sampled_individuals.append(self.population[sampled_arg])
@@ -62,17 +62,26 @@ class GeneticPath(object):
         genotype_arg_1, genotype_arg_2 = genotype_args
         fitness_value_1, fitness_value_2 = fitness_values
         if fitness_value_1 >= fitness_value_2:
-            genotype_arg_2 = genotype_arg_1.copy()
-            genotype_arg_2 = self.mutate(self.population[genotype_arg_2])
+            genotype_path_1 = self.population[genotype_arg_1].copy()
+            self.population[genotype_arg_2] = genotype_path_1 #self.mutate(genotype_path_1)
         else:
-            genotype_arg_1 = genotype_arg_2.copy()
-            genotype_arg_1 = self.mutate(self.population[genotype_arg_1])
+            genotype_path_2 = self.population[genotype_arg_2].copy()
+            self.population[genotype_arg_1] = genotype_path_2 #self.mutate(genotype_path_2)
 
 if __name__ == '__main__':
     genetic_paths = GeneticPath(shape=(5, 3))
-    path_1, path_2 = genetic_paths.sample_genotype_paths()
+    paths, path_args = genetic_paths.sample_genotype_paths()
+    path_1, path_2 = paths
     print('path_1 \n', path_1)
-    #print('path_2 \n', path_2)
-    mutated_path_1 = genetic_paths.mutate(path_1)
+    mutated_path_1 = genetic_paths.mutate(path_1).copy()
     print('mutated_path_1 \n', mutated_path_1)
-
+    """
+    loss = (100, 1)
+    print(loss)
+    genetic_paths.overwrite(path_args, loss)
+    print('overwritten_path_1 (winner): \n',
+            genetic_paths.population[path_args[0]])
+    print('overwritten_path_2 (loser): \n',
+            genetic_paths.population[path_args[1]])
+    print(genetic_paths.population[path_args[0]] == genetic_paths.population[path_args[1]])
+    """
