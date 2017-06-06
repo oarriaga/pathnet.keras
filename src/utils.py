95afb2c1bf7dd3e2c5bb016ac5b390450fdb0215
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-06 15:20:13 +0200

    Fixed bug with split data funnction

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def normalize_images(image_array):
-    return image_array / 255.
+    return image_array.astype('float32') / 255.
 
 def display_image(image_array, image_class, cmap=None):
     image_array = np.squeeze(image_array)
@@ -19,10 +19,10 @@ def display_image(image_array, image_class, cmap=None):
 def split_data(train_data, validation_split=.2):
     num_train = int(round((1 - validation_split) * len(train_data[0])))
     train_images, train_classes = train_data
+    validation_images = train_images[num_train:]
+    validation_classes = train_classes[num_train:]
     train_images = train_images[:num_train]
     train_classes = train_classes[:num_train]
-    validation_images = train_data[num_train:]
-    validation_classes = train_data[num_train:]
     train_data = (train_images, train_classes)
     validation_data = (validation_images, validation_classes)
     return train_data, validation_data
