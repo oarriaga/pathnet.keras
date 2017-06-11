9eef71f8a138221fe95cad8a0fcbaf2868732716
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-11 17:11:03 +0200

    Implemented reset_weights, from_path_to_names functions and added frozen_paths variable to save function

--- src/utils.py
+++ src/utils.py
@@ -8,13 +8,6 @@ import numpy as np
 def normalize_images(image_array):
     return image_array.astype('float32') / 255.
 
-
-def spice_up_images(images):
-    num_samples = len(images)
-    for sample_arg in range(num_samples):
-        images[sample_arg] = _add_salt_and_pepper(images[sample_arg])
-    return images
-
 def _add_salt_and_pepper(image_array, probability=.5):
     image_array = np.squeeze(image_array)
     uniform_values = np.random.rand(*image_array.shape)
@@ -26,13 +19,11 @@ def _add_salt_and_pepper(image_array, probability=.5):
     spiced_image[salty_mask] = np.max(image_array)
     return spiced_image
 
-def display_image(image_array, image_class, cmap=None):
-    image_array = np.squeeze(image_array)
-    image_array = image_array.astype('uint8')
-    plt.imshow(image_array, cmap)
-    image_class = str(image_class)
-    plt.title('ground truth class: ' + image_class)
-    plt.show()
+def spice_up_images(images):
+    num_samples = len(images)
+    for sample_arg in range(num_samples):
+        images[sample_arg] = _add_salt_and_pepper(images[sample_arg])
+    return images
 
 def split_data(train_data, validation_split=.2):
     num_train = int(round((1 - validation_split) * len(train_data[0])))
@@ -45,9 +36,31 @@ def split_data(train_data, validation_split=.2):
     validation_data = (validation_images, validation_classes)
     return train_data, validation_data
 
-def save_layer_weights(model, save_path):
+def display_image(image_array, image_class, cmap=None):
+    image_array = np.squeeze(image_array)
+    image_array = image_array.astype('uint8')
+    plt.imshow(image_array, cmap)
+    image_class = str(image_class)
+    plt.title('ground truth class: ' + image_class)
+    plt.show()
+
+def from_path_to_names(paths):
+    if (len(paths) is 0) or (paths is None):
+        return []
+    frozen_path_names = []
+    for path in paths:
+        path_coordinates = list(zip(*[coordinates.tolist()
+                        for coordinates in np.where(path)]))
+        names = ['module_' + str(args[0]) + str(args[1])
+                        for args in path_coordinates]
+        frozen_path_names + frozen_path_names + names
+    return list(set(frozen_path_names))
+
+def save_layer_weights(model, save_path, frozen_paths=[]):
+    frozen_path_names = from_path_to_names(frozen_paths)
     for layer in model.layers:
-        if 'reduce' in layer.name:
+        layer_name = layer.name
+        if ('reduce' in layer_name) or (layer_name in frozen_path_names):
             continue
         file_path = save_path + layer.name + '.p'
         pickle.dump(layer.get_weights(), open(file_path, 'wb'))
@@ -63,6 +76,16 @@ def load_layer_weights(model, save_path):
             weights_path = save_path + layer.name + '.p'
             weights = pickle.load(open(weights_path, 'rb'))
             layer.set_weights(weights)
+
+def reset_weights(frozen_paths, save_path):
+    frozen_path_names = from_path_to_names(frozen_paths)
+    file_paths = glob.glob(save_path + '*.p')
+    weight_filenames = [os.path.basename(path)[:-2] for path in file_paths]
+    for weight_filename in weight_filenames:
+        if weight_filename in frozen_path_names:
+            continue
+        else:
+            os.remove(save_path + weight_filename + '.p')
 
 def to_categorical(data):
     arg_classes = np.unique(data).tolist()
