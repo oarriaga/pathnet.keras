4b7b2fb1b50adb3ca82add5b4f29f05ad803689c
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-17 12:24:46 +0200

    Added instantiation of the classes and model at every experiment iteration

--- src/mnist_experiment.py
+++ src/mnist_experiment.py
@@ -12,26 +12,25 @@ num_layers = 3
 num_modules_per_layer = 10
 num_neurons_per_module = 20
 output_size = 2
-genetic_agents = GeneticAgents(shape=(num_modules_per_layer, num_layers))
-pathnet = PathNet(shape=(num_modules_per_layer, num_layers),
-                num_neurons_per_module=num_neurons_per_module,
-                output_size=output_size)
-
 # parameters for the task manager
 class_arg_list = [[4, 5], [6, 7]]
 val_accuracy_treshold = .90
 dataset_name = 'mnist'
 save_path = '../trained_models/'
-num_experiments = 2
+num_experiments = 200
 genetic_epochs = []
-task_manager = TaskManager(pathnet, genetic_agents,
-                            dataset_name, class_arg_list,
-                            accuracy_treshold=val_accuracy_treshold)
 for experiment_arg in range(num_experiments):
     print('Experiment:', experiment_arg)
+    genetic_agents = GeneticAgents(shape=(num_modules_per_layer, num_layers))
+    pathnet = PathNet(shape=(num_modules_per_layer, num_layers),
+                num_neurons_per_module=num_neurons_per_module,
+                output_size=output_size)
+    task_manager = TaskManager(pathnet, genetic_agents,
+                            dataset_name, class_arg_list,
+                            accuracy_treshold=val_accuracy_treshold)
     reset_all_weigths(save_path)
     task_manager.train_tasks()
     genetic_epochs.append(task_manager.genetic_epochs)
     K.clear_session()
-pickle.dump(genetic_epochs, open('mnist_experiment.p', 'wb'))
+pickle.dump(genetic_epochs, open('mnist_results.p', 'wb'))
 
